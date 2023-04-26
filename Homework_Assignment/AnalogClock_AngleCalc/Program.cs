dbf7ec4879c7ae4cf2a4df1296a5ec61264a6b59
Mikael Aggarao <104477933+Kael-09@users.noreply.github.com>
2023-04-26 16:48:16 +0800

    Add files via upload

---
+++ Homework_Assignment/AnalogClock_AngleCalc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AnalogClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Analog Clock Angle Calculator\n");
+            
+            // 1-12 only for the hour hand, greater than this will result to negative degrees
+            Console.Write("Enter the hour (1-12): ");
+            int hour = int.Parse(Console.ReadLine());
+
+            // 0-59 only for the minute hand, greater than this will result to negative degress even if the hour hand is between 1-12
+            Console.Write("Enter the minute (0-59): ");
+            int minute = int.Parse(Console.ReadLine());
+
+            // Calculate the angles in degrees of the hour and minute hands
+            // Formula for computing hourAngle = 0.5 * (60 * hour + minute); 0.5 because the hour hand moves 30 degrees in 1 hour or 0.5 degrees per minute 
+            double hourAngle = 0.5 * (60 * hour + minute);
+
+            // Formula for computing minuteAngle = 6 * minute; 6 because the minute hand moves 360 degrees in 60 minutes or 6 per minute
+            double minuteAngle = 6 * minute;
+
+            // Calculate the absolute difference between the two angles
+            double angleDiff = Math.Abs(hourAngle - minuteAngle);
+
+            // Take the lesser angle between the two possible angles
+            angleDiff = Math.Min(angleDiff, 360 - angleDiff);
+
+            // Output the result to the console window
+            Console.WriteLine($"The time is {hour} : {minute}");
+            Console.WriteLine($"The lesser angle between the hour and minute hands is {angleDiff} degrees.");
+        }
+    }
+}
---
+++ Homework_Assignment/Nodes_Recursion/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nodes_Recursion
+{
+    class Branch
+    {
+        public List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the hierarchical structure
+            Branch root = new Branch();
+            Branch level1a = new Branch();
+            Branch level1b = new Branch();
+            Branch level2a = new Branch();
+            Branch level2b = new Branch();
+            Branch level2c = new Branch();
+            Branch level3a = new Branch();
+            Branch level3b = new Branch();
+            Branch level3c = new Branch();
+            Branch level4 = new Branch();
+
+            root.branches.Add(level1a);
+            root.branches.Add(level1b);
+            level1a.branches.Add(new Branch());
+            level1b.branches.Add(level2a);
+            level1b.branches.Add(level2b);
+            level1b.branches.Add(level2c);
+            level2a.branches.Add(new Branch());
+            level2b.branches.Add(new Branch());
+            level2c.branches.Add(new Branch());
+            level3a.branches.Add(new Branch());
+            level3b.branches.Add(new Branch());
+            level3c.branches.Add(new Branch());
+            level3b.branches.Add(level4);
+
+            // Calculate the depth of the hierarchical structure
+            // Counting from 0 to 4; this gives depth of 4
+            int depth = CalculateDepth(root);
+            Console.WriteLine($"The depth of the hierarchical structure is {depth}");
+        }
+
+        static int CalculateDepth(Branch branch)
+        {
+            if (branch.branches.Count == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                int maxDepth = 0;
+                foreach (Branch child in branch.branches)
+                {
+                    int childDepth = CalculateDepth(child);
+                    if (childDepth > maxDepth)
+                    {
+                        maxDepth = childDepth;
+                    }
+                }
+                return maxDepth + 1;
+            }
+        }
+    }
+}
